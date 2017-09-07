e883c31fa92ee505f4f26e6ee692d8cf75997e06
sreymompnc <sreymom.mann@gmail.com>
2017-09-07 11:34:46 +0700

    new project

---
+++ app/src/main/java/ruc/ps_app_project/ImageProfileActivity.java
@@ -0,0 +1,92 @@
+package ruc.ps_app_project;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import cz.msebera.android.httpclient.Header;
+
+public class ImageProfileActivity extends AppCompatActivity {
+    Context context;
+    ImageView profile;
+    TextView back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_profile);
+        context = ImageProfileActivity.this;
+        profile = (ImageView)findViewById(R.id.imageView_pro);
+
+        SharedPreferences preferLogin = getSharedPreferences("loginInfo", Context.MODE_PRIVATE);
+        String userId = preferLogin.getString("userId","");
+        final String userName = preferLogin.getString("userName","");
+
+        Toast.makeText(ImageProfileActivity.this, "profile",Toast.LENGTH_SHORT).show();
+        final AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://192.168.1.6:8888/posters/posterProfile/"+userId, new AsyncHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    String data = new String(responseBody, "UTF8");
+                    //Log.i("data", data);
+                    try {
+                        JSONObject obj = new JSONObject(data);
+                        //String status = obj.getString("status");
+                        JSONObject poster_data= obj.getJSONObject("posterProfile");
+                        String profiles = poster_data.getString("image");
+                        // profile poster
+                        final String posterUrlImg = "http://192.168.1.6:8888/images/posters/"+profiles;
+                        loadProfile(posterUrlImg,profile);
+
+                    }catch (JSONException e){
+                        e.printStackTrace();
+                    }
+                }catch (UnsupportedEncodingException e){
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+
+        back = (TextView) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+    //============================ To load image of profile==============================================
+    private void loadProfile(String url,ImageView imgView){
+        Picasso.with(context)
+                .load(url)
+                .resize(800,800)
+                .centerInside()// to zoom img
+                //.centerCrop()
+                .into(imgView);
+    }
+    //============================ for
+    public void onBackPressed()
+    {
+        // code here to show dialog
+        super.onBackPressed();  // optional depending on your needs
+    }
+
+}
